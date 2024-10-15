3dc4e67d4a762e74a0924ef9bac0b40a9d457d88
Dhriti <dhriti.baruah20@gmail.com>
2024-10-15 22:09:00 +0530

    changed model

--- src/app/api/openAI/route.js
+++ src/app/api/openAI/route.js
@@ -1,34 +1,26 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
-const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY });
 
 export async function POST(req, res) {
-
   if (req.method !== "POST") {
     return NextResponse.json({ message: "Method not allowed" });
   }
   try {
     const { prompt } = await req.json();
-    const response = await openai.chat.completions.create(
-      {
-        model: "gpt-4o-mini",
-        messages: [
-            { role: "system", content: "You are a helpful assistant." },
-            {
-                role: "user",
-                content: prompt,
-            },
-        ],
-        max_tokens: 100,
-      },
-    
-    );
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo", // Or 'gpt-3.5-turbo'
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: prompt },
+      ],
+      max_tokens: 100,
+    });
 
-    console.log(response);
-    return NextResponse.json(
-      { text: "response.data.choices[0].message.content" },
-      { status: 200 }
-    );
+    // Access the generated text from the response
+    const generatedText = completion.choices[0].message.content;
+    console.log(generatedText);
+    return NextResponse.json({ text: generatedText }, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,30 @@
-import { pool } from "@/utils/dbConnect"
-import dbConnect from "@/utils/dbConnect"
-import Link from "next/link"
-import { redirect } from "next/navigation"
-
+import { pool } from "@/utils/dbConnect";
+import dbConnect from "@/utils/dbConnect";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
- 
-
   return (
-   <main className="p-10 bg-black flex justify-center items-center w-full h-screen ">
-    <div className="flex-col space-y-4">
-    <div>
-        <Link href='/notes'>
-          <button className="p-2 w-28 text-white bg-cyan-700">Notes</button>
-        </Link>
-    </div>
-    <div>
-        <Link href='/tesseract'>
-          <button className="p-2 w-28 text-white bg-cyan-700">Tesseract</button>
-        </Link>
-    </div>
-    <div>
-        <Link href='/openAPI'>
-          <button className="p-2 w-28 text-white bg-cyan-700">OpenAPI</button>
-        </Link>
-    </div>
-    </div>
-   </main>
-  )
+    <main className="p-10 bg-black flex justify-center items-center w-full h-screen ">
+      <div className="flex-col space-y-4">
+        <div>
+          <Link href="/notes">
+            <button className="p-2 w-28 text-white bg-cyan-700">Notes</button>
+          </Link>
+        </div>
+        <div>
+          <Link href="/tesseract">
+            <button className="p-2 w-28 text-white bg-cyan-700">
+              Tesseract
+            </button>
+          </Link>
+        </div>
+        <div>
+          <Link href="/openAPI">
+            <button className="p-2 w-28 text-white bg-cyan-700">OpenAPI</button>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
